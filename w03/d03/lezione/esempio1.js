da0f6eebc4e9043190724b1691321ea57bf56310
DESKTOP-J6UKQTF\Blaze <apassaglia91@gmail.com>
2021-07-07 12:07:29 +0200

    ovewrites

--- w03/d03/lezione/esempio1.js
+++ w03/d03/lezione/esempio1.js
@@ -1,8 +1,12 @@
-var input=36;
+var input =0;
 
-if(input>0){
+if (input > 0) {
     console.log("positivo");
 }
-else if(input<0){
-    console.log("negativo")
+else if (input < 0) {
+    console.log("negativo");
 }
+else {
+    console.log("uguale a 0");
+}
+console.log("finito");
