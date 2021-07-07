0487bb302feed38308c4c8a5a652c036949aacd4
DESKTOP-J6UKQTF\Blaze <apassaglia91@gmail.com>
2021-07-07 14:08:56 +0200

    if & switch

--- w03/d03/lezione/esempio1.js
+++ w03/d03/lezione/esempio1.js
@@ -1,5 +1,6 @@
 var input = 9;
 
+// if è più versatile
 if (input > 0) {
     console.log("positivo");
 }
@@ -10,3 +11,17 @@ else {
     console.log("uguale a 0");
 }
 console.log("finito");
+
+
+// lo switch si usa solo per uguaglianze
+switch (x) {
+    case 1:
+        console.log("uno");
+        break;
+    case 2:
+        console.log("due");
+        break;
+    default:
+        console.log("qualcos'altro")
+
+}
